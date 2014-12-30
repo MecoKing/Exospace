766bdd74507af446b1ad283d7d54ee2daf543011
Micah Merswolke <mecoking@icloud.com>
2014-12-30 11:59:34 -0500

    Initial commit for Exospace

---
+++ GameScene.swift
@@ -0,0 +1,29 @@
+//
+//  GameScene.swift
+//  Exospace
+//
+//  Created by [pixelmonster] on 2014-12-30.
+//  Copyright (c) 2014 antitypical. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+	
+	var world = Array<Tile> ()
+	
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
