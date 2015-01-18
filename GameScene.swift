2c464719947da5397ed5cc8d6bbc964fde002067
Micah Merswolke <mecoking@icloud.com>
2015-01-18 09:29:35 -0500

    Made people walk around aimlessly one tile at a time...

--- GameScene.swift
+++ GameScene.swift
@@ -97,6 +97,13 @@ class GameScene: SKScene {
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
 		timerTick++
+		if timerTick % 50 == 0 {
+			for human in population {
+				if human.state == "idle" {
+					human.pathFind()
+				}
+			}
+		}
 		if timerTick % 5 == 0 {
 			for human in population {
 				human.animate()
--- Person.swift
+++ Person.swift
@@ -12,6 +12,7 @@ import SpriteKit
 class Person : SKSpriteNode {
 	var cartesianPoint:CGPoint
 	var animFrame:CGRect
+	var state = "idle"
 	
 	init(atPoint:CGPoint, spriteName:String) {
 		animFrame = CGRect(x: 0, y: 0, width: 0.25, height: 1)
@@ -38,11 +39,39 @@ class Person : SKSpriteNode {
 		return Person(atPoint: pt, spriteName: people [index])
 	}
 	
+	func pathFind () {
+		let direction = random() % 4
+		if direction == 0 {
+			moveTo(CGPoint(x: cartesianPoint.x + 1, y: cartesianPoint.y))
+		}
+		else if direction == 1 {
+			moveTo(CGPoint(x: cartesianPoint.x - 1, y: cartesianPoint.y))
+		}
+		else if direction == 2 {
+			moveTo(CGPoint(x: cartesianPoint.x, y: cartesianPoint.y + 1))
+		}
+		else {
+			moveTo(CGPoint(x: cartesianPoint.x, y: cartesianPoint.y - 1))
+		}
+	}
+	
 	func animate () {
 		animFrame.origin.x += 0.25
 		if animFrame.origin.x >= 1 {
 			animFrame.origin.x = 0
 		}
 		texture = SKTexture(rect: animFrame, inTexture: SKTexture(imageNamed: name!))
+	}
+	
+	func moveTo (cartesian:CGPoint) {
+		if cartesian.x >= 0 && cartesian.x <= 11 && cartesian.y >= 0 && cartesian.y <= 11 {
+			state = "walking"
+			let isoLocation = CGPoint(x: cartesian.toUsefulIsometric().x, y: cartesian.toUsefulIsometric().y + 44)
+			let moveTime = position.distanceFrom(isoLocation) / 12
+			runAction(SKAction .moveTo(isoLocation, duration: NSTimeInterval(moveTime))) {
+				self.cartesianPoint = cartesian
+				self.state = "idle"
+			}
+		}
 	}
 }
