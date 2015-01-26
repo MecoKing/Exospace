da3f17fc5a54454634561026095ae3f7d33b184d
Micah Merswolke <mecoking@icloud.com>
2015-01-26 15:44:11 -0500

    Made cavepeople stop wearing hats
    Hypothetical hats…

--- Outfit.swift
+++ Outfit.swift
@@ -11,10 +11,12 @@ import SpriteKit
 
 class Outfit : SKSpriteNode {
 	
-	var animFrame: CGRect;
+	var animFrame: CGRect
+	let hasHair: Bool
 	
-	init(spriteName:String) {
+	init(spriteName:String, hasHeadpiece:Bool) {
 		animFrame = CGRect(x: 0, y: 0, width: 0.25, height: 0.25)
+		hasHair = hasHeadpiece
 		let image = SKTexture(rect: animFrame, inTexture: SKTexture(imageNamed: spriteName))
 		super.init(texture: image, color: SKColor.clearColor(), size: CGSize(width: 24, height: 24))
 		name = spriteName
@@ -33,10 +35,14 @@ class Outfit : SKSpriteNode {
 	}
 	
 	//Get a random job name
-	class func randomJob () -> String {
+	class func randomOutfitForGender ( gender:String) -> Outfit {
 		let path = NSBundle.mainBundle().pathForResource("Jobs", ofType: "plist")
 		let jobs:Array<Dictionary<String, String>> = NSArray (contentsOfFile: path!) as Array<Dictionary<String, String>>
-		return jobs [World.randomInt(jobs.count)] ["Name"]!
+		let jobIndex = World.randomInt(jobs.count)
+		return Outfit(
+			spriteName: jobs [jobIndex]["Name"]! + gender,
+			hasHeadpiece: (jobs [jobIndex]["Headpiece"]! == "YES") ? true : false
+		)
 	}
 	
 }
