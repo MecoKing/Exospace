f8d04709d68a94054d75719cdee72338a471e4b1
Micah Merswolke <mecoking@icloud.com>
2015-01-29 16:05:05 -0500

    Buttons now highlight when selected

--- Button.swift
+++ Button.swift
@@ -12,19 +12,33 @@ import SpriteKit
 class Button : SKSpriteNode {
 	
 	var selected = false
+	let selectBox = SKSpriteNode(texture: SKTexture(imageNamed: "buttonSelect"), color: SKColor.clearColor(), size: CGSize(width: 96, height: 96))
 	let job:String
 	
 	init (buttonName:String, index:Int) {
 		let jobs = ["diceRollButton":"randomWorld", "buildButton":"buildItems"]
 		if jobs [buttonName] != nil { job = jobs [buttonName]! }
 		else { job = "noJob" }
+		selectBox.texture?.filteringMode = SKTextureFilteringMode.Nearest
 		super.init(texture: SKTexture(imageNamed: buttonName), color: SKColor.clearColor(), size: CGSize(width: 96, height: 96))
 		name = buttonName
 		position = CGPoint(x: 48 + (96 * index), y: 144)
+		texture?.filteringMode = SKTextureFilteringMode.Nearest
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
 	    fatalError("init(coder:) has not been implemented")
+	}
+	
+	func select () {
+		if !selected {
+			addChild(selectBox)
+			selected = true
+		}
+	}
+	func deselect () {
+		removeChildrenInArray([selectBox])
+		selected = false
 	}
 	
 	func runAction () {
